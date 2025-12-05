f181207e9b953fbd0738582345eb6c2fd9c399d7
keincarrillo <kein.carrillo.escom@gmail.com>
2025-12-05 09:29:36 -0600

    feat: enum de errores

--- src/types/createProduct.ts
+++ src/types/createProduct.ts
@@ -1,11 +1,16 @@
 import type { Request } from 'express'
+import type { DocumentData } from 'firebase/firestore'
 
-export interface ProductBody {
-  nombre: string
-  marca: string
-  precio: number
-  descripcion: string
-  stock: number
+export interface ProductBody extends DocumentData {
+  nombre?: string
+  marca?: string
+  precio?: number
+  descripcion?: string
+  stock?: number
 }
 
-export interface Product extends Request<{}, any, ProductBody> {}
+export interface ProductParams {
+  id: string
+}
+
+export interface Product extends Request<ProductParams, any, ProductBody> {}
---
+++ src/types/errorsProduct.ts
@@ -0,0 +1,5 @@
+export const enum Error {
+  NO_DATA,
+  MISSING_DATA,
+  NO_PARAMS,
+}
