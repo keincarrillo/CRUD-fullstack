66e32473f0ee22b04bc7bd4d3d652640be9ae6cb
keincarrillo <kein.carrillo.escom@gmail.com>
2025-12-07 18:16:54 -0600

    feat: manejo de errores en signup

--- src/controllers/authentication.controllers.ts
+++ src/controllers/authentication.controllers.ts
@@ -1,8 +1,10 @@
 import type { Request, Response } from 'express'
 import type { AuthParamsReq } from '../types/authParams'
+import type { FirebaseError } from 'firebase/app'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../firebase/appFirebase'
 import { validateAuthBody } from '../utils/validateAuthBody'
+import { errorsResponseAuth } from '../utils/errorsResponseAuth'
 
 export const singUp = async (req: AuthParamsReq, res: Response) => {
   const validationError = validateAuthBody(req.body)
@@ -14,8 +16,12 @@ export const singUp = async (req: AuthParamsReq, res: Response) => {
       req.body.email,
       req.body.password
     )
+
     res.sendStatus(201)
-  } catch (error) {
+  } catch (err) {
+    const error = err as FirebaseError
+    const errorResponse = errorsResponseAuth(error)
+    if (errorResponse) return res.status(400).json({ message: errorResponse })
     console.error(error)
     res.status(500).send(error)
   }
---
+++ src/types/errorsResponseAuth.ts
@@ -0,0 +1,5 @@
+export const enum Error {
+  EMAIL_ALREADY_IN_USE = 'Email already in use',
+  INVALID_EMAIL = 'Invalid email',
+  WEAK_PASSWORD = 'Weak password',
+}
