a2e188c8e210d2152a2f6024a4b96062f60dc46f
neik0_o <137359799+keincarrillo@users.noreply.github.com>
2025-12-07 22:33:51 -0600

    refactor: Simplify validation logic in validateUpdateProduct

--- src/utils/validateUpdateProduct.ts
+++ src/utils/validateUpdateProduct.ts
@@ -2,7 +2,5 @@ import type { ProductBodyReq } from '../types/createProduct'
 import { Error } from '../types/errorsProduct'
 
 export const validateUpdateProduct = (req: ProductBodyReq) => {
-  if (!req.body || Object.keys(req.body).length === 0) {
-    return Error.NO_DATA
-  }
+  if (!req.body || Object.keys(req.body).length === 0) return Error.NO_DATA
 }
