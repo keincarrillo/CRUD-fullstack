54bd52b3b862eed024003ee3f1b88e607e318786
KeinPC <kein.carrillo.escom@gmail.com>
2025-12-18 23:02:45 -0600

    fix: redis

--- server/src/redis/clientRedis.ts
+++ server/src/redis/clientRedis.ts
@@ -5,7 +5,7 @@ const redisUrl = process.env.REDIS_URL ?? 'redis://127.0.0.1:6379'
 const client = createClient({ url: redisUrl })
 
 client.on('error', error => {
-  console.log(`Redis Client Error: ${error}`)
+  console.log(`Redis Client Err: ${error}`)
 })
 
 await client.connect()
