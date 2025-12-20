883fe3f2df8526e65a85cfe7819ef470d5fa790a
keincarrillo <kein.carrillo.escom@gmail.com>
2025-12-20 13:32:45 -0600

    feat: servicios de autenticacion y signout

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -4,8 +4,10 @@ import type { SigninFormData } from '../types/signinFormType'
 
 axios.defaults.withCredentials = true
 
+const API_URL = 'http://localhost:3000/api/auth'
+
 export const signupRequest = async (data: SignupFormData) => {
-  const res = await axios.post('http://localhost:3000/api/auth/signup', {
+  const res = await axios.post(`${API_URL}/signup`, {
     name: data.name,
     email: data.email,
     password: data.password,
@@ -14,9 +16,19 @@ export const signupRequest = async (data: SignupFormData) => {
 }
 
 export const signinRequest = async (data: SigninFormData) => {
-  const res = await axios.post('http://localhost:3000/api/auth/signin', {
+  const res = await axios.post(`${API_URL}/signin`, {
     email: data.email,
     password: data.password,
   })
+  return res.data
+}
+
+export const verifyAuth = async () => {
+  const res = await axios.get(`${API_URL}/verify`)
+  return res.data
+}
+
+export const signout = async () => {
+  const res = await axios.post(`${API_URL}/signout`)
   return res.data
 }
